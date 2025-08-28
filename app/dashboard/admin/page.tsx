22653fe326fff3bb8665449189cd3ac019e42f06
Shorno <fb.shorno@gmail.com>
2025-08-29 03:36:06 +0600

    feat(add categories)

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,13 +1,7 @@
-"use client"
-import TestUpload from "@/components/TestUpload";
-import CloudinaryUploadWidget from "@/components/cloudinary-upload-widget";
-
 export default function AdminDashboardPage() {
     return (
         <div>
            admin dashboard page
-            <TestUpload/>
-            <CloudinaryUploadWidget onChange={(url) => console.log(url)}/>
         </div>
     )
 }
