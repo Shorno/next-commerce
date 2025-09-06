e65e6aaa2f3f373691c8f560ad568b2535bf5439
Shorno <fb.shorno@gmail.com>
2025-09-06 12:12:25 +0600

    optimize redirect logic

--- app/dashboard/seller/(store)/stores/[slug]/layout.tsx
+++ app/dashboard/seller/(store)/stores/[slug]/layout.tsx
@@ -22,12 +22,14 @@ export default async function SellerStoreLayout({children, params}: SellerStoreL
         redirect("/dashboard/seller/stores/new")
     }
 
-    // Check if the slug parameter matches one of the user's stores
+    // Check if the slug parameter matches one of the user's stores...
     const store = userStores.find(store => store.slug === slug)
 
     if (!store) {
         notFound()
     }
+
+
 
     return (
         <>
